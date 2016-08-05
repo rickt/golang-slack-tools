b507dd30388e690af98b93ac9cdd1396e0840642
rick tait <rickt@rickt.org>
2016-08-05 09:45:20 -0700

    Create slackminisniffer.go

---
+++ slackminisniffer.go
@@ -0,0 +1,21 @@
+package main
+import (
+        "fmt"
+        "github.com/nlopes/slack"
+)
+func main() {
+        // create new slack object & connect
+        api := slack.New("TOKEN_REDACTED")
+        api.SetDebug(true)
+        rtm := api.NewRTM()
+        // lets do it
+        go rtm.ManageConnection()
+        // loop() and listen for inbound messages
+        for {
+                select {
+                case msg := <-rtm.IncomingEvents:
+                        fmt.Print("Event Received: ")
+                        fmt.Printf("  %s\n", msg.Data)
+                }
+        }
+}
