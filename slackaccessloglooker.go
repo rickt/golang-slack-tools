1f2d422e018abd544cab694747c390cc72299f92
rick tait <rickt@rickt.org>
2016-09-29 09:03:56 -0700

    a comment

--- slackaccessloglooker.go
+++ slackaccessloglooker.go
@@ -1,80 +1,81 @@
 package main
 
 import (
-   "encoding/json"
-   "fmt"
-   "log"
-   "net/http"
-   "strings"
-   "time"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type SlackAccessLog struct {
-   Status     bool                  `json:"ok"`
-   Logins     []SlackAccessLogEntry `json:"logins"`
-   PagingData SlackAccessLogPaging  `json:"paging"`
+	Status     bool                  `json:"ok"`
+	Logins     []SlackAccessLogEntry `json:"logins"`
+	PagingData SlackAccessLogPaging  `json:"paging"`
 }
 
 type SlackAccessLogEntry struct {
-   UserID    string `json:"user_id"`
-   Username  string `json:"username"`
-   DateFirst int64  `json:"date_first"`
-   DateLast  int64  `json:"date_last"`
-   Count     int    `json:"count"`
-   IP        string `json:"ip"`
-   UserAgent string `json:"user_agent"`
-   ISP       string `json:"isp"`
-   Country   string `json:"country"`
-   Region    string `json:"region"`
+	UserID    string `json:"user_id"`
+	Username  string `json:"username"`
+	DateFirst int64  `json:"date_first"`
+	DateLast  int64  `json:"date_last"`
+	Count     int    `json:"count"`
+	IP        string `json:"ip"`
+	UserAgent string `json:"user_agent"`
+	ISP       string `json:"isp"`
+	Country   string `json:"country"`
+	Region    string `json:"region"`
 }
 
 type SlackAccessLogPaging struct {
-   Count int `json:"count"`
-   Total int `json:"total"`
-   Page  int `json:"page"`
-   Pages int `json:"pages"`
+	Count int `json:"count"`
+	Total int `json:"total"`
+	Page  int `json:"page"`
+	Pages int `json:"pages"`
 }
 
 // helper func to do a case-insensitive search
 func caseinsensitivecontains(a, b string) bool {
-   return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
+	return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
 }
 
 var (
-   page     int    = 1
-   pages    int    = 101
-   token    string = "REDACTED"
-   slackurl string = "https://slack.com/api/team.accessLogs"
+	page     int    = 1
+	pages    int    = 101
+	token    string = "REDACTED"
+	slackurl string = "https://slack.com/api/team.accessLogs"
 )
 
 func main() {
-   var sal SlackAccessLog
-   // 100 pages of JSON max from the team.accessLogs Slack API
-   for page := 1; page < pages; page++ {
-      // build the url
-      url := fmt.Sprintf("%s?token=%s&page=%d", slackurl, token, page)
-      // create the request
-      req, err := http.NewRequest("GET", url, nil)
-      if err != nil {
-         log.Fatal("error: %s", err)
-      }
-      // create the http client
-      client := &http.Client{}
-      // get the response
-      response, err := client.Do(req)
-      if err != nil {
-         log.Fatal("error: %s", err)
-      }
-      defer response.Body.Close()
-      // decode the JSON response into our SlackAccessLog var
-      if err := json.NewDecoder(response.Body).Decode(&sal); err != nil {
-         log.Println(err)
-      }
-      // range through this page of the response and ignore Slack App/Android/iPhone useragents
-      for _, dj := range sal.Logins {
-         if !(caseinsensitivecontains(dj.UserAgent, "Slack_SSB") || caseinsensitivecontains(dj.UserAgent, "Android") || caseinsensitivecontains(dj.UserAgent, "iPhone") || caseinsensitivecontains(dj.UserAgent, "iPad") || caseinsensitivecontains(dj.UserAgent, "ApiApp")) {
-            fmt.Printf("%s\t%s\t%s\n", dj.Username, time.Unix(dj.DateLast, 0), dj.UserAgent)
-         }
-      }
-   }
+	var sal SlackAccessLog
+	// 100 pages of JSON max from the team.accessLogs Slack API
+	for page := 1; page < pages; page++ {
+		// build the url
+		url := fmt.Sprintf("%s?token=%s&page=%d", slackurl, token, page)
+		// create the request
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal("error: %s", err)
+		}
+		// create the http client
+		client := &http.Client{}
+		// get the response
+		response, err := client.Do(req)
+		if err != nil {
+			log.Fatal("error: %s", err)
+		}
+		defer response.Body.Close()
+		// decode the JSON response into our SlackAccessLog var
+		if err := json.NewDecoder(response.Body).Decode(&sal); err != nil {
+			log.Println(err)
+		}
+		// range through this page of the response
+		for _, dj := range sal.Logins {
+			// the useragents i'm ignoring are: mobile devices and [best guess] Slack native clients
+			if !(caseinsensitivecontains(dj.UserAgent, "Slack_SSB") || caseinsensitivecontains(dj.UserAgent, "Android") || caseinsensitivecontains(dj.UserAgent, "iPhone") || caseinsensitivecontains(dj.UserAgent, "iPad") || caseinsensitivecontains(dj.UserAgent, "ApiApp")) {
+				fmt.Printf("%s\t%s\t%s\n", dj.Username, time.Unix(dj.DateLast, 0), dj.UserAgent)
+			}
+		}
+	}
 }
--- slackminisniffer.go
+++ slackminisniffer.go
@@ -1,21 +1,23 @@
 package main
+
 import (
-        "fmt"
-        "github.com/nlopes/slack"
+	"fmt"
+	"github.com/nlopes/slack"
 )
+
 func main() {
-        // create new slack object & connect
-        api := slack.New("TOKEN_REDACTED")
-        api.SetDebug(true)
-        rtm := api.NewRTM()
-        // lets do it
-        go rtm.ManageConnection()
-        // loop() and listen for inbound messages
-        for {
-                select {
-                case msg := <-rtm.IncomingEvents:
-                        fmt.Print("Event Received: ")
-                        fmt.Printf("  %s\n", msg.Data)
-                }
-        }
+	// create new slack object & connect
+	api := slack.New("TOKEN_REDACTED")
+	api.SetDebug(true)
+	rtm := api.NewRTM()
+	// lets do it
+	go rtm.ManageConnection()
+	// loop() and listen for inbound messages
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			fmt.Printf("  %s\n", msg.Data)
+		}
+	}
 }
